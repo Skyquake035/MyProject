a1c8d8c89901f8feb397d6bbc8bc24599381d5e1
Skyquake035 <48541671+Skyquake035@users.noreply.github.com>
2020-08-20 15:38:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Layout } from 'antd';
-import { Popover} from 'antd';
-
-const content = (
-  <div>
-    <p>Content</p>
-    <p>Content</p>
-  </div>
-);
+import { Select, Layout } from 'antd';
 
 const { Footer } = Layout;
+
+const { Option } = Select;
+
+const emailRegex = RegExp();
 
 const formValid = ({ formErrors, ...rest }) => {
   let valid = true;
@@ -28,6 +24,22 @@ const formValid = ({ formErrors, ...rest }) => {
   return valid;
 };
 
+function onChange(value) {
+  console.log(`selected ${value}`);
+}
+
+function onBlur() {
+  console.log('blur');
+}
+
+function onFocus() {
+  console.log('focus');
+}
+
+function onSearch(val) {
+  console.log('search:', val);
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -36,10 +48,24 @@ class App extends Component {
       
       username: null,
       password: null,
+      confirmPassword: null,
+      prefix: null,
+      firstName: null,
+      lastName: null,
+      email: null,
+      treatment: null,
+      gender: null,
 
       formErrors: {
         username:  "",
         password:  "",
+        confirmPassword:  "",
+        prefix:  "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        treatment:  "",
+        gender:  "",
       }
     };
   }
@@ -52,6 +78,13 @@ class App extends Component {
         --SUBMITTING--
         Username:  ${this.state.username}
         Password: ${this.state.password}
+        Confirm Password:  ${this.state.confirmPassword}
+        Prefix:  ${this.state.prefix}
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        Email: ${this.state.email}
+        Treatment:  ${this.state.treatment}
+        Gender:  ${this.state.gender}
       `);
     } else {
       console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
@@ -72,6 +105,15 @@ class App extends Component {
         formErrors.password =
           value.length < 6 ? "minimum 6 characaters required" : "";
         break;
+      case "confirmPassword":
+        formErrors.firstName =
+          value.length < 6 ? "minimum 6 characaters required" : "";
+        break;  
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
       
       default:
         break;
@@ -87,7 +129,7 @@ class App extends Component {
       <Layout>
       <div className="wrapper">
         <div className="form-wrapper">
-          <h1>เข้าสู่ระบบ</h1>
+          <h1>ลงทะเบียน</h1>
           <form onSubmit={this.handleSubmit} noValidate>
             <div className="username">
                 <label htmlFor="username">ชื่อผู้ใช้</label>
@@ -117,14 +159,124 @@ class App extends Component {
                 <span className="errorMessage">{formErrors.password}</span>
               )}
             </div>
+            <div className="confirmPassword">
+              <label htmlFor="password">ยืนยันรหัสผ่าน</label>
+              <input
+                className={formErrors.password.length > 0 ? "error" : null}
+                placeholder="ยืนยันรหัสผ่าน"
+                type="password"
+                name="password"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.password.length > 0 && (
+                <span className="errorMessage">{formErrors.password}</span>
+              )}
+            </div>
+
+            <div className="prefix">
+            <label htmlFor="prefix">คำนำหน้า</label>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder="คำนำหน้า"
+              optionFilterProp="prefix"
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              onSearch={onSearch}
+              filterOption={(input, option) =>
+                option.prefix.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+              <Option value="mr">นาย</Option>
+              <Option value="ms">นาง</Option>
+              <Option value="mrs">นางสาว</Option>
+            </Select>
+            </div>
+
+            <div className="firstName">
+              <label htmlFor="firstName">ชื่อ</label>
+              <input
+                className={formErrors.firstName.length > 0 ? "error" : null}
+                placeholder="ชื่อ"
+                type="text"
+                name="firstName"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.firstName.length > 0 && (
+                <span className="errorMessage">{formErrors.firstName}</span>
+              )}
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">นามสกุล</label>
+              <input
+                className={formErrors.lastName.length > 0 ? "error" : null}
+                placeholder="นามสกุล"
+                type="text"
+                name="lastName"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.lastName.length > 0 && (
+                <span className="errorMessage">{formErrors.lastName}</span>
+              )}
+            </div>
+            <div className="email">
+              <label htmlFor="email">อีเมล</label>
+              <input
+                className={formErrors.email.length > 0 ? "error" : null}
+                placeholder="อีเมล"
+                type="email"
+                name="email"
+                noValidate
+                onChange={this.handleChange}
+              />
+              {formErrors.email.length > 0 && (
+                <span className="errorMessage">{formErrors.email}</span>
+              )}
+            </div>
+            <br />
+
+            <div className="treatment">
+            <label htmlFor="treatment">ศูนย์การรักษา</label>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder="ศูนย์การรักษา"
+              optionFilterProp="treatment"
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              onSearch={onSearch}
+              filterOption={(input, option) =>
+                option.treatment.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+              <Option value="1">1</Option>
+              <Option value="2">2</Option>
+              <Option value="3">3</Option>
+            </Select>
+            </div>
+
+            <div className="gender">
+            <label htmlFor="gender">เพศ</label>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder="เพศ"
+              optionFilterProp="gender"
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              onSearch={onSearch}
+              filterOption={(input, option) =>
+                option.gender.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+              <Option value="male">ชาย</Option>
+              <Option value="female">หญิง</Option>
+            </Select>
+            </div>
             
             <div className="createAccount">
-              <button type="submit">เข้าสู่ระบบ</button>
-              <Popover content={content} title="Title" trigger="hover">
-                <small>ลืมรหัสผ่าน?</small>
-              </Popover>
-              <small>ลงทะเบียน</small>
-              
+              <button type="submit">ลงทะเบียน</button>
+              <small>มีบัญชีผู้ใช้อยู่แล้ว?</small>
             </div>
           </form>
         </div>
